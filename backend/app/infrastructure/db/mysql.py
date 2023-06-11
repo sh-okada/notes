caef9e64c6d3ebe0bce124860f6261ac017dee78
sh-okada <sh-okada@ymail.ne.jp>
2023-06-11 21:36:24 +0900

    db名変更

--- backend/app/infrastructure/db/mysql.py
+++ backend/app/infrastructure/db/mysql.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:root@db:3306/qa?charset=utf8"
+SQLALCHEMY_DATABASE_URL = "mysql://root:root@db:3306/notes?charset=utf8"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
