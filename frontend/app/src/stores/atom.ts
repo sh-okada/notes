8e338fb03866fbf45eaa7f0d94e619f434b8a9ef
sh-okada <sh-okada@ymail.ne.jp>
2023-06-11 18:16:20 +0900

    コメント削除

--- frontend/app/src/stores/atom.ts
+++ frontend/app/src/stores/atom.ts
@@ -2,6 +2,6 @@ import { Message } from "@/types/state";
 import { atom } from "recoil";
 
 export const messageState = atom<Message | undefined | null>({
-  key: "messageState", // unique ID (with respect to other atoms/selectors)
-  default: undefined, // default value (aka initial value)
+  key: "messageState",
+  default: undefined,
 });
