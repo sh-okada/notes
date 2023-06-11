494ad53ddff54f056014577a29158454c55c93c5
sh-okada <sh-okada@ymail.ne.jp>
2023-06-11 19:08:01 +0900

    fab追加

--- frontend/app/src/pages/notes/index.tsx
+++ frontend/app/src/pages/notes/index.tsx
@@ -2,28 +2,46 @@ import { getNotes } from "@/api";
 import NotesItem from "@/components/molecules/NotesItem";
 import { NoteResponse } from "@/types/api";
 import { withCookie, withValidToken } from "@/utils/middleware";
-import { Grid } from "@mui/material";
+import { Fab, Grid, Stack } from "@mui/material";
 import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { BsPencilSquare } from "react-icons/bs";
 
 interface Props {
   notes: NoteResponse[];
 }
 
 export default function Page({ notes }: Props) {
+  const router = useRouter();
+
+  const handleClickFab = () => {
+    router.push("/notes/edit");
+  };
+
   return (
-    <Grid container spacing={3}>
-      {notes.map((note) => {
-        return (
-          <Grid key={note.id} item xs={12} md={6}>
-            <NotesItem
-              id={note.id}
-              title={note.title}
-              createdAt={note.created_at}
-            />
-          </Grid>
-        );
-      })}
-    </Grid>
+    <Stack>
+      <Grid container spacing={3}>
+        {notes.map((note) => {
+          return (
+            <Grid key={note.id} item xs={12} md={6}>
+              <NotesItem
+                id={note.id}
+                title={note.title}
+                createdAt={note.created_at}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Fab
+        color="primary"
+        size="large"
+        onClick={handleClickFab}
+        sx={{ position: "absolute", bottom: 32, right: 32 }}
+      >
+        <BsPencilSquare />
+      </Fab>
+    </Stack>
   );
 }
 
