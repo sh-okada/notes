d0ca89e63fda924ce6d2b421200c309787d48aab
sh-okada <sh-okada@ymail.ne.jp>
2023-06-11 18:16:01 +0900

    サーバーサイド実行時にAPIURLを変えるように修正

--- frontend/app/src/libs/axios.ts
+++ frontend/app/src/libs/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const customAxios = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL:
+    typeof window === "undefined"
+      ? "http://backend:8000"
+      : "http://localhost:8000",
   withCredentials: true,
 });
