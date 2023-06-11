8987211013727110ceb8c78e105c3646d90f6c48
sh-okada <sh-okada@ymail.ne.jp>
2023-06-11 18:15:21 +0900

    ボタン押下処理実装

--- frontend/app/src/components/organisms/Header.tsx
+++ frontend/app/src/components/organisms/Header.tsx
@@ -1,14 +1,37 @@
-import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  ButtonBase,
+  Container,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+
+  const handleClickHome = () => {
+    router.push("/");
+  };
+
+  const handleClickEdit = () => {
+    router.push("/notes/edit");
+  };
+
   return (
     <AppBar elevation={0} color="inherit">
       <Container maxWidth="md">
         <Toolbar variant="dense" disableGutters>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Notes
-          </Typography>
-          <Button variant="contained" size="small">
+          <Box sx={{ flexGrow: 1 }}>
+            <ButtonBase onClick={handleClickHome}>
+              <Typography variant="h6" fontWeight="bold">
+                Notes
+              </Typography>
+            </ButtonBase>
+          </Box>
+          <Button variant="contained" size="small" onClick={handleClickEdit}>
             メモを取る
           </Button>
         </Toolbar>
